33673a8c9cb0541750ab45752eb0b4343def9362
RayBrandServices <30844229+RayBrandServices@users.noreply.github.com>
2021-11-17 20:49:46 -0600

    somewhat More than barebones
    somewhat more than barebones

--- part4/FileManipulator.java
+++ part4/FileManipulator.java
@@ -18,28 +18,39 @@ public static void main(String[] args) {
    
 }
 
+
+static public int Result(String Input) {
+
+while(Input != null)
+{
+
+}
+
+return 0;
+}
+
 static void RESET() {
 System.out.println("DO NOTHING BUT RESET");
 }
 
 static void ADD() {
-System.out.println("DO NOTHING BUT RESET");
+System.out.println(" ADD");
 }
 
 static void SUB() {
-System.out.println("DO NOTHING BUT RESET");
+System.out.println(" SUB ");
 }
 
 static void MUL() {
-System.out.println("DO NOTHING BUT RESET");
+System.out.println(" MUL ");
 }
 
 static void DIV() {
-System.out.println("DO NOTHING BUT RESET");
+System.out.println(" DIV ");
 }
 
 static void MOD() {
-System.out.println("DO NOTHING BUT RESET");
+System.out.println(" MOD ");
 }
 
     public static void readTextFileUsingScanner(String fileName)
@@ -52,44 +63,47 @@ System.out.println("DO NOTHING BUT RESET");
             String line;
             while ((line = br.readLine()) != null) {
               System.out.println(line);
-              for(int i = 0; i < line.length(); i++)
-              {
+             //for(int i = 0; i < line.length(); i++)
+            // {
              String reset = "0000";
              String add   = "0001";
              String sub   = "0010";
              String mult  = "0011";
              String div   = "0100";
-             String mod = "0101";
-             if(line == reset) {
+             String mod   = "0101";
+             
+             if(line.equals(reset)) {
              System.out.println("RESET");
+             
              RESET();
              }
-             if(line == add) {
-             System.out.println("RESET");
+             if(line.equals(add)) {
+             System.out.println("ADD");
             //Read next line to get the digits
              ADD();
              }            
-             if(line == sub) {
-             System.out.println("RESET");
+             if(line.equals(sub)) {
+             System.out.println("SUB");
             //Read next line to get the digits
              SUB();
              }
-             if(line == mult) {
-             System.out.println("RESET");
+             if(line.equals(mult)) {
+             System.out.println("MULT");
             //Read next line to get the digits
              MUL();
              }
-             if(line == div) {
-             System.out.println("RESET");
+             if(line.equals(div)) {
+             System.out.println("DIV");
             //Read next line to get the digits
              DIV();
              }
-             if(line == mod) {
-             System.out.println("RESET");
+             if(line.equals(mod)) {
+             System.out.println("MOD");
             //Read next line to get the digits
              MOD();
              }
-              }
+            //  }
+             
             }
           }
           catch(IOException e) { e.printStackTrace();}
@@ -98,6 +112,9 @@ System.out.println("DO NOTHING BUT RESET");
               try { if (br != null) br.close(); }
               catch(IOException e) { e.printStackTrace(); }
           }
+       
+       
+       
     }
    
 
